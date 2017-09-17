4a8d37df13b2db7b33c33504422c723547af8152
Font77 <vimalkg@gmail.com>
2017-09-17 15:05:04 +0530

    added turtlekuij

---
+++ studentController.js
@@ -0,0 +1,6 @@
+mainApp.controller("kakntrolr", function($scope) {
+   $scope.kadata = [
+         ['1)li = [3, 5, 10]. li[-3] is : ? --- hint negative indeks starts from last.', "None", "Error", "0", "3"],
+         ['2)property in python is : ? --- hint @propety', "class", "built", "attribute", "decorator"],
+      ]
+});
