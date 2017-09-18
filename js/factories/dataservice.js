899039e8635f0cb559db39bbb89002a73f87b06c
Font77 <vimalkg@gmail.com>
2017-09-18 07:57:34 +0530

    added turtle based kuij

---
+++ js/factories/dataservice.js
@@ -0,0 +1,28 @@
+(function(){
+    angular.module("turtleFacts").factory("DataService", DataService);
+    function DataService(){
+        var dataObj = {
+            turtlesData: turtlesData,
+            quizQuestions: quizQuestions,
+            correctAnswers: correctAnswers
+        };
+        return dataObj;
+    }
+    var correctAnswers = [3, 3];
+    var quizQuestions  = [
+        {
+            type: "text", text: "1)li = [3, 5, 10]. li[-3] is : ? --- hint negative indeks starts from last.",
+            possibilities: [ { answer: "none" }, { answer: "error" }, { answer: "0" } , { answer: "3" }],
+            selected: null,
+            correct: null
+        },
+        {
+            type: "text", text: "2)property in python is : ? --- hint @propety",
+            possibilities: [ { answer: "class" }, { answer: "function" }, { answer: "attribute" }, { answer: "decorator" } ],
+            selected: null,
+            correct: null
+        },
+    ];
+   var turtlesData = [ ];
+
+})();
