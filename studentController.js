cb13fc5f8749d33cfd5d35296e864f9d3923f4d9
Font77 <vimalkg@gmail.com>
2017-09-18 07:52:37 +0530

    deleted old files

--- studentController.js
+++
@@ -1,6 +0,0 @@
-mainApp.controller("kakntrolr", function($scope) {
-   $scope.kadata = [
-         ['1)li = [3, 5, 10]. li[-3] is : ? --- hint negative indeks starts from last.', "None", "Error", "0", "3"],
-         ['2)property in python is : ? --- hint @propety', "class", "built", "attribute", "decorator"],
-      ]
-});
