c88a0769adeb47060cec0f9ddb4bd092704bc4d7
Font77 <vimalkg@gmail.com>
2017-09-20 09:14:51 +0530

    Added GetSelectedLesson

--- js/controllers/list.js
+++ js/controllers/list.js
@@ -1,7 +1,7 @@
 (function(){
     angular.module("learn_facts").controller("listCtrl", ListController);
-    ListController.$inject = ['quizMetrics', 'DataService'];
-    function ListController(quizMetrics, DataService){
+    ListController.$inject = ['$scope','quizMetrics', 'DataService'];
+    function ListController($scope, quizMetrics, DataService){
         var vm = this;
         vm.quizMetrics = quizMetrics; // Controllers reference to the quiz data from factory
         // vm.data = DataService.kyuij_viki; // Controller reference to the turtle info created in the factory
@@ -9,14 +9,15 @@
         vm.changeActiveTurtle = changeActiveTurtle; // reference to a named function below
         vm.activateQuiz = activateQuiz; // reference to named function below
         vm.search = ""; // will hold the search query when user uses search bar in view
-        vm.sbjAkts = {
+        $scope.sbjAkts = {
             'python': {'level1': ['builtin functions', 'list'],'level2': ['tuple', 'dictionary'],'level3': ['sets', 'dictionary']},
             'knARa': { 'level1': ['numbers', 'pronouns', 'kolors', 'motions'], 'level2': ['vegetables', 'kitchen'], 'level3': ['Jaipur', 'Ajmer', 'Jodhpur'] },
             'Angulrjs': { 'New South Wales': ['Sydney'], 'Victoria': ['Melbourne'] }
         };
-        vm.GetSelectedCountry = function() { vm.strCountry = vm.sbjAktSrc; };
-        vm.GetSelectedState = function() { vm.strState = vm.levelSrc; };        
-        function changeActiveTurtle(index){ vm.activeTurtle = index; } 
+        $scope.GetSelectedSubject = function() { $scope.strSubject = $scope.sbjAktSrc; };
+        $scope.GetSelectedLevel = function() { $scope.strLevel = $scope.levelSrc; };
+        $scope.GetSelectedLesson = function() { $scope.strLesson = $scope.lessonSrc; };
+        function changeActiveTurtle(index){ vm.activeTurtle = index; }
         function activateQuiz(){ quizMetrics.changeState("quiz", true); }
     }
 })();
