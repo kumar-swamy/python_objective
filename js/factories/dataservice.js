6af32f9ec38d7e3a3f27c1478f998ce0756cab0f
viml qumar <viml@vnindia.in>
2017-09-22 12:54:01 +0000

    trying to select 10 random kuestions

--- js/factories/dataservice.js
+++ js/factories/dataservice.js
@@ -9,85 +9,102 @@
             setkyuij: setkyuijfunc
         };
         return dataObj;
-        function setkyuijfunc(argsbjAkt, arglevel, arglesson){
+        function setkyuijfunc(argsbjAkt, arglevel, arglesson, n=10){
+            // if (arglesson === "all")
+            // dataObj.kyuijKuestions = getRandom(kyuijKuestionsSrc[argsbjAkt][arglevel][arglesson]['kuestion_options'],n);
+            // else
             dataObj.kyuijKuestions = kyuijKuestionsSrc[argsbjAkt][arglevel][arglesson]['kuestion_options'];
-            dataObj.krAkt_ansrs = kyuijKuestionsSrc[argsbjAkt][arglevel][arglesson]['ansrs'];
+            
+            //dataObj.krAkt_ansrs = kyuijKuestionsSrc[argsbjAkt][arglevel][arglesson]['ansrs'];
+            //dataObj.krAkt_ansrs = getAnsers(dataObj.kyuijKuestions);
+            dataObj.krAkt_ansrs = dataObj.kyuijKuestions.map(i=>i[5]);
+            //alert(dataObj.krAkt_ansrs);
+        }
+        /*function getAnsers(arr) {
+            var result = new Array(arr.length);
+            for(indeks = 0; indeks < arr.length; indeks++) {
+                result[indeks] = arr[indeks][5];
+            }
+            return result;
+        }*/
+        function getRandom(arr, n) {
+            var result = new Array(n),
+                len = arr.length,
+                taken = new Array(len);
+            if (n > len)
+                throw new RangeError("getRandom: more elements taken than available");
+            while (n--) {
+                var x = Math.floor(Math.random() * len);
+                result[n] = arr[x in taken ? taken[x] : x];
+                taken[x] = --len;
+            }
+            return result;
         }
     }
     var kyuijKuestionsSrc  = {
         "python":{
             "level1":{
-                "lesson1":{
+                "all":{
                     'kuestion_options': [
                         [
                             "text",
                             "li = [3, 5, 10]. li[-3] is : ? --- hint negative indeks starts from last.",
                             ["none", "error", "0", "3"],
-                            null,
-                            null
+                            null, null, 3
                         ],
                         [
                             "text",
                             "vhich vil giv error? --- hint dict key cannot be mutable",
                             ['dic = { [1,2,3]:"abc"}', 'dic = { (1,2,3):"abc", 3.1415:"abc"}', "a = []", "b = ()"],
-                            null,
-                            null
+                            null, null, 0
                         ],
                         [
                             "text",
                             " try : value = my_dict[key]. if key dont ekist then hao tu handle",
                             ['error: pass', 'except KeyError: pass', "else:", "try:"],
-                            null,
-                            null
+                            null, null, 1
                         ],
                         [
                             "text",
                             " my_dict is a dict. no of key/value pairs in my_dict = ",
                             ['#my_dict', 'my_dict.length()', "len(my_dict)", "my_dict.length"],
-                            null,
-                            null
+                            null, null, 2
                         ],
                         [
                             "text",
                             " my_dict is a dict. to delete a key (k) & get is value vithout KeyError  ve kn use : ",
                             ['val = del my_dict[k]', 'val = my_dict.pop("k")', 'val = my_dict.pop("k", None)', 'val = del my_dict[k,None]'],
-                            null,
-                            null
+                            null, null, 2
                         ],
                         [
                             "text",
                             ' d1 = {"k1" :2, "k2" :3, "k3" :4} . d2 = {"k4" :2, "k5" :3} . to AAd d2 key/value pairs in d1 : ',
                             ['d1 = d1 + d2', 'd1.update(d2)', 'd1.add(d2)', 'd1.push(d2)'],
-                            null,
-                            null
+                            null, null, 1
                         ],
                         [
                             "text",
                             'vhich vil giv error ?',
                             ['d1 = {}', 'd1 = {0 : "k1", 1 : "k2" }', 'd1 = {"k1" : 2, "k1" : 5}', 'd1 = None'],
-                            null,
-                            null
+                            null, null, 2
                         ],
                         [
                             "text",
                             'd = {"a":123, "b":34, "c":304, "d":99} ? vhich is inkorrekt ?',
                             ['for key in d:', 'for value in d.values():', 'for value in d.value():', 'for key in d.keys():'],
-                            null,
-                            null
+                            null, null, 2
                         ],
                         [
                             "text",
                             'keylist = ["a", "b", "c", "d"] , vallist = [1, 2, 3, 4] ? vhich is korrekt ?',
                             ['d = dict(keylist : vallist)', 'd = zip(keylist, vallist)', 'd = d.keys():d.values()', 'd = dict(zip(keylist, vallist))'],
-                            null,
-                            null
+                            null, null, 3
                         ],
                         [
                             "text",
                             "property in python is : ? --- hint @property",
                             ["class", "function", "attribute", "decorator"],
-                            null,
-                            null
+                            null, null, 3
                         ]
                     ],
                     'ansrs': [3, 0, 1, 2, 2, 1, 2, 2, 3, 3]
@@ -200,6 +217,13 @@
                     'kuestion_options': [
                         [
                             "text",
+                            "which is quadratic equation below ?",
+                            ['ax<sup>2</sup> + bx + c = 0', 'ax<sup>2</sup> + bx + c', 'x<sup>2</sup> + y<sup>2</sup> = c', 'ax + by -c = 0'],
+                            null,
+                            null
+                        ],
+                        [
+                            "text",
                             "ax<sup>2</sup> + bx + c = 0 ; D(diskriminant) = &#8710; = ?",
                             ['b<sup>2</sup> - 4a', 'c<sup>2</sup> - 4ab', 'b<sup>2</sup> - 4ac', 'a<sup>2</sup> - 4bc'],
                             null,
@@ -212,8 +236,57 @@
                             null,
                             null
                         ],
+                        [
+                            "text",
+                            "ax<sup>2</sup> + bx + c = 0 ; &#8710; = b<sup>2</sup> - 4ac . its roots (r1, r2) are : ",
+                            ["( -b &plusmn; &radic;&#8710; ) / 2a", "( -b &plusmn; &radic;&#8710; )/a", "( b &plusmn; &radic;&#8710; ) / 2a", "( -b &plusmn; &radic;&#8710; ) / 2c"],
+                            null,
+                            null
+                        ],
+                        [
+                            "text",
+                            "In ax<sup>2</sup> + bx + c = 0  , b<sup>2</sup> - 4ac = 0. then roots are",
+                            ["positive", "negative", "imaginary", "equal"],
+                            null,
+                            null
+                        ],
+                        [
+                            "text",
+                            "In ax<sup>2</sup> + bx + c = 0  , b<sup>2</sup> - 4ac < 0. then roots are",
+                            ["positive", "negative", "imaginary", "equal"],
+                            null,
+                            null
+                        ],
+                        [
+                            "text",
+                            "In ax<sup>2</sup> + bx + c = 0  , b<sup>2</sup> - 4ac > 0. then roots are",
+                            ["positive", "real", "imaginary", "equal"],
+                            null,
+                            null
+                        ],
+                        [
+                            "text",
+                            "In x<sup>2</sup> - 9x + 20 = 0  , then roots are",
+                            ["4, 5", "4, 7", "7 , 8", "5, 7"],
+                            null,
+                            null
+                        ],                        
+                        [
+                            "text",
+                            "sum of root is s and product of root is p. then ekuation is :",
+                            ["ax<sup>2</sup> + sx + p = 0", "x<sup>2</sup> + sx + p = 0", "x<sup>2</sup> - sx + p = 0", "x<sup>2</sup> + sx - p = 0"],
+                            null,
+                            null
+                        ],                        
+                        [
+                            "text",
+                            "25x<sup>2</sup> - 25x + 4 = 0. its roots are :",
+                            ["3, 4", "2/3, 4/5", "1/5, 4/5", "6, 0"],
+                            null,
+                            null
+                        ],
                     ],
-                    'ansrs': [2, 3]
+                    'ansrs': [0, 2, 3, 0, 3, 2, 1, 0, 2, 2]
                 },
             }
         },        
